541602cad2704024997abb4efaac106bf1831333
Julius Goryavsky <julius.goryavsky@mariadb.com>
2022-03-04 04:18:55 +0100

    MDEV-19281: Porting hashicorp plugin from ES edition
    Also improved error handling during plugin initialization
    (memory deallocation is performed in case of failure).

--- plugin/hashicorp_key_management/hashicorp_key_management_plugin.cc
+++ plugin/hashicorp_key_management/hashicorp_key_management_plugin.cc
@@ -1,4 +1,4 @@
-/* Copyright (C) 2019-2021 MariaDB Corporation
+/* Copyright (C) 2019-2022 MariaDB Corporation
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -918,7 +918,6 @@ static int hashicorp_key_management_plugin_init(void *p)
   const static size_t x_vault_token_len = strlen(x_vault_token);
   char *token_env= getenv("VAULT_TOKEN");
   size_t token_len = strlen(token);
-  local_token = NULL;
   if (token_len == 0)
   {
     if (token_env)
@@ -989,16 +988,9 @@ static int hashicorp_key_management_plugin_init(void *p)
   {
     my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER
                     "Memory allocation error", 0);
-    return 1;
+    goto Failure2;
   }
   snprintf(token_header, buf_len, "%s%s", x_vault_token, token);
-  curl_global_init(CURL_GLOBAL_ALL);
-  list = curl_slist_append(list, token_header);
-  if (list == NULL)
-  {
-    my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER
-                    "curl: unable to construct slist", 0);
-  }
   vault_url_len = strlen(vault_url);
   /*
     Checking the maximum allowable length to protect
@@ -1009,6 +1001,15 @@ static int hashicorp_key_management_plugin_init(void *p)
     my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER
                     "Maximum allowed vault URL length exceeded",
                     0);
+Failure:
+    free(token_header);
+    token_header = NULL;
+Failure2:
+    if (local_token)
+    {
+      free(local_token);
+      local_token = NULL;
+    }
     return 1;
   }
   if (vault_url_len && vault_url[vault_url_len - 1] == '/')
@@ -1024,12 +1025,22 @@ static int hashicorp_key_management_plugin_init(void *p)
   {
     my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER
                     "Memory allocation error", 0);
-    return 1;
+    goto Failure;
   }
   memcpy(vault_url_data, vault_url, vault_url_len);
   memcpy(vault_url_data + vault_url_len, "/data/", 7);
   cache_max_time = ms_to_ticks(cache_timeout);
   cache_max_ver_time = ms_to_ticks(cache_version_timeout);
+  /* Initialize curl: */
+  curl_global_init(CURL_GLOBAL_ALL);
+  list = curl_slist_append(list, token_header);
+  if (list == NULL)
+  {
+    my_printf_error(ER_UNKNOWN_ERROR, PLUGIN_ERROR_HEADER
+                    "curl: unable to construct slist", 0);
+    curl_global_cleanup();
+    goto Failure;
+  }
   return 0;
 }
 
